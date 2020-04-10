2485dd28ac7295cced729fe65a2240f14aecc1b4
donjoe <giumontanaro@gmail.com>
2020-04-10 16:32:23 +0200

    fix authHeader error with get blogs request

--- middleware/tokenExtractor.js
+++ middleware/tokenExtractor.js
@@ -1,15 +1,17 @@
 const tokenExtractor = (req, res, next) => {
     const authHeader = req.get('Authorization')
     
-    if (!authHeader) {
+    if (req.method !== 'GET' && !authHeader) {
         const error = new Error('Authorization header missing')
         error.name = 'AuthorizationError'
         return next(error)
     }
     
-    const token = authHeader.split(' ')[1]
-    if(token) {
-        req.token = token
+    if(authHeader) {
+        const token = authHeader.split(' ')[1]
+        if(token) {
+            req.token = token
+        }
     }
 
     next()
